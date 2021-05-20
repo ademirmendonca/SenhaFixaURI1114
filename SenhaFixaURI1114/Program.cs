58e95153c66786e215df72966849df179fd86f34
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-05-20 00:03:07 -0300

    Add project files.

---
+++ SenhaFixaURI1114/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SenhaFixaURI1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer a leitura de uma senha, caso ela seja diferente de 2002
+            //Exibir a mensagem: Senha Invalida. Caso a senha estiver correta: acesso permitido
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
